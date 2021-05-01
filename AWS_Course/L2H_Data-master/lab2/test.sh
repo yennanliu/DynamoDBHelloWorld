a94ffa8203b14430f1e618697253cc51c7e5b20e
yennanliu <f339339@gmail.com>
2021-05-01 19:12:42 +0800

    update

---
+++ AWS_Course/L2H_Data-master/lab2/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Read Throttle
+for i in {1..100};
+do
+    echo 'i = ' $i
+
+    aws dynamodb scan \
+     --table-name WeatherData \
+     --filter-expression "StationId = :stationId" \
+     --expression-attribute-values '{":stationId":{"S":"C0A9F0"}}'
+done
+
+
+# Weite Throttle
+for i in {1..50};
+do
+    aws dynamodb batch-write-item --request-items file://WeatherData3.json
+done
