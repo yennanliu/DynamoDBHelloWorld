35fa174ad07b957bac6128e738cc80231cae465a
yennanliu <f339339@gmail.com>
2021-05-01 18:57:52 +0800

    update

--- AWS_Course/L2H_Data-master/lab3/all_RH_with_time.sh
+++ AWS_Course/L2H_Data-master/lab3/all_RH_with_time.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # All RH at time (ObsTime) 13
+
+# https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/LegacyConditionalParameters.AttributesToGet.html
+# https://docs.aws.amazon.com/cli/latest/reference/dynamodb/query.html
+
+aws dynamodb scan \
+     --table-name WeatherData \
+     --filter-expression "ObsTime = :obsTime" \
+     --expression-attribute-values '{ ":obsTime": {"N":"13"} }'
